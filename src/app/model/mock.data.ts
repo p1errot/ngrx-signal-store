9245edc66554235f06677c9808e8eaf26ddc9f39
Samuel Rivera <samuelp1errot@gmail.com>
2024-11-08 10:10:44 +0500

    Tailwind and signal store installed

---
+++ src/app/model/mock.data.ts
@@ -0,0 +1,29 @@
+import { Todo } from './todo.model';
+
+export const TODOS: Todo[] = [
+  {
+    id: 1,
+    title: 'Get milk',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'Get bread',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: 'Get eggs',
+    completed: false,
+  },
+  {
+    id: 4,
+    title: 'Get cheese',
+    completed: false,
+  },
+  {
+    id: 5,
+    title: 'Get water',
+    completed: false,
+  },
+];
